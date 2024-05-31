b31bcbfcabc4ae5bebfc7be0451b56d7b667a3b5
Raki47 <167620297+Raki47@users.noreply.github.com>
2024-06-01 07:16:15 +0800

    Update and rename m_colour.h to r47_colour.h

---
+++ B-League-Football/engine/R47/r47_colour.h
@@ -0,0 +1,94 @@
+// Raki47 - Colour palette
+
+/*
+Copyright (c) 2024 Filip Rancic
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+*/
+// 01/06/2024
+// Originally I wrote all the colour definitions by using hex values, now I have changed It to '0xRRGGBB' format to ensure that It also works In the MASM version of this file
+
+// R47 GUI special colours
+// In the "special" colours section I have listed colours that stood out to me In particular, whether It's for looking modern, attention grabbing or generally aesthetically pleasing
+#define LIME 0x00FF00
+#define GHOSTWHITE 0xF8F8FF
+// Had to Include cyan as homage to Terry A. Davis. Rest In peace, your contributions to software engineering created despite your significant personal/mental struggles will forever be an Inspiration.
+#define CYAN 0x00FFFF
+#define ALICEBLUE 0xF0F8FF
+#define LIGHTCYAN 0xE0FFFF
+#define LAVENDER 0xE6E6FA
+#define GAINSBORO 0xDCDCDC
+#define LIGHTGREY 0xD3D3D3
+#define SILVER 0xC0C0C0
+#define GREY 0xA9A9A9
+#define DARKLIGHTGREY 0x4D4D4D
+#define DARKMIDGREY 0x3B3A3A
+#define DARKGREY 0x212121
+#define RAKIGREY 0x474747
+#define BLUEVIOLET 0x8A2BE2
+#define DARKVIOLET 0x9400D3
+#define CRIMSON 0xDC143C
+#define GOLDENROD 0xDAA520
+#define DARKGOLDENROD 0xB8860B
+#define DARKORANGE 0xFF8C00
+
+// Basic colours
+#define WHITE 0xFFFFFF
+#define SNOW 0xFFFAFA
+#define LINEN 0xFAF0E6
+#define ANTIQUEWHITE 0xFAEBD7
+#define NAVAJOWHITE 0xFFDEAD
+#define CADETBLUE 0x5F9EA0
+#define STEELBLUE 0x4682B4
+#define CORNFLOWERBLUE 0x6495ED
+#define DEEPSKYBLUE 0x00BFFF
+#define DARKTURQUOISE 0x00CED1
+#define ROYALEBLUE 0x4169E1
+#define MEDIUMBLUE 0x0000CD
+#define BLUE 0x0000FF
+#define DARKBLUE 0x00008B
+#define NAVY 0x000080
+#define MIDNIGHTBLUE 0x191970
+#define PURPLE 0x800080
+#define DARKMAGENTA 0x8B008B
+#define DARKORCHID 0x9932CC
+#define INDIGO 0x4B0082
+#define MAGENTA 0xFF00FF
+#define ORCHID 0xDA70D6
+#define MEDIUMORCHID 0xBA55D3
+#define VIOLET 0xEE82EE
+#define LIGHTSALMON 0xFFA07A
+#define LIGHTCORAL 0xF08080
+#define LIGHTRED 0xFF4040
+#define RED 0xFF0000
+#define DARKRED 0x8B0000
+#define MAROON 0x800000
+#define ORANGE 0xFFA500
+#define ORANGERED 0xFF4500
+#define BURLYWOOD 0xDEB887
+#define ROSYBROWN 0xBC8F8F
+#define TOMATO 0xFF6347
+#define INDIANRED 0xCD5C5C
+#define FIREBRICK 0xB22222
+#define BROWN 0xA52A2A
+#define SIEANNA 0xA0522D
+#define OLIVE 0x808000
+#define GOLD 0xFFD700
+#define YELLOW 0xFFFF00
+#define YELLOWGREEN 0x9ACD32
+#define GREENYELLOW 0xADEE2E
+#define GREEN 0x328032
+#define SEAGREEN 0x3CB371
+#define SPRINGGREEN 0x00FF7F
+#define DARKSEAGREEN 0x8FBC8F
+#define DIMGREY 0x696969
+#define BLACK 0x000000
